e570d6d062639726f764d4a58fd3ebeff8a6a1d1
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-01-27 13:51:30 +0100

    initial commit

---
+++ src/test/java/com/example/moviecharacterapi/MovieCharacterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviecharacterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCharacterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
