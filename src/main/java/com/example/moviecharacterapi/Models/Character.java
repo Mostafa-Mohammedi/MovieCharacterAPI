1c6dbb1663aa4e36ec9c18e93e0107523a433c6b
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-01-29 16:10:57 +0100

    changed the naming of the database name and finished the insertion of franchise and Character

--- src/main/java/com/example/moviecharacterapi/Models/Character.java
+++ src/main/java/com/example/moviecharacterapi/Models/Character.java
@@ -11,13 +11,11 @@ public class Character {
     @Column(name = "ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int character_Id;
-    @Column(name = "actor_Name", length = 250, nullable = false)
+    @Column(name = "actor_name", length = 250, nullable = false)
     private String fullName;
 
-    @Column(name = "character_Name", length = 250, nullable = true)
     private String alias;
 
-    @Column(name = "genderType", length = 250, nullable = true)
     private String gender;
 
     @Column(name = "picture")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,24 @@
+
+
+-- insertion of the movies of avenger
+
+
+
 insert into movie (title, genre, release_year, director, picture, trailer) values ('The Avengers', 'action, fantasy', '2012', 'Joss Whedon', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt0848228%2F&psig=AOvVaw2wFadSMjRgGkotWW3FtHpo&ust=1675086865374000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCMC0n__27PwCFQAAAAAdAAAAABAD', 'https://www.youtube.com/watch?v=aXgoJs7_HDY');
 
 insert into movie (title, genre, release_year, director, picture, trailer) values ('Avengers age of Ultron', 'Action, Fantasy', '2015', 'Joss Whedon', 'https://static.tvtropes.org/pmwiki/pub/images/91s8hbxxbtl_ri.jpg', 'https://www.youtube.com/watch?v=P5iIPfNzj2o');
 
 insert into movie (title, genre, release_year, director, picture, trailer) values ('Avengers Infinity War', 'Action, Fantasy', '2018', 'Anthony Russo', 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersinfinitywar_lob_mas_hlf_01_3.jpg', 'https://www.youtube.com/watch?v=6ZfuNTqbHE8');
+
+
+insert into franchise (name, description) values('Marvel', 'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios.');
+
+
+-- insertion of the character
+
+
+insert into character (actor_name, alias, gender, picture) values ('Chris Evans','Captain America', 'man', 'https://cdn.britannica.com/30/182830-050-96F2ED76/Chris-Evans-title-character-Joe-Johnston-Captain.jpg');
+
+insert into character (actor_name, alias, gender, picture) values ('Robert John Downey Jr','Tony Stark', 'man', 'https://goat.com.au/wp-content/uploads/2019/10/tony-stark-snap-avengers-endgame.jpg');
+
 
